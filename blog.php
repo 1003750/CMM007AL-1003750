61982aa0c2ce12e8f1366b31ef62e8843a02c79c
1003750 <1003750@rgu.ac.uk>
2016-05-04 12:06:33 +0100

    i

--- blog.php
+++ blog.php
@@ -63,7 +63,11 @@
             $entrySummary = $row['entrySummary'];
             $category = $row['category'];
             $submitter = $row['submitter'];
-            echo "<h3>{$entryTitle} by {$submitter}</h3>";
+            echo "<h3>{$entryTitle} by ";
+                if(isset($_GET['$submitter'])) {
+                    $submitter = $_GET['submitter'];
+                    echo "<h3>{$sumbitter}</h3>";
+                }
             echo "<h4>{$category}</h4>";
             echo "<p>{$entrySummary}</p>";
             echo "<hr>";
